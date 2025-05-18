fd7e076e6748249fa9363c18d08e775022acdb29
navin <kartikmaurya18@gmail.com>
2025-05-18 12:02:42 +0530

    commit

--- src/main/java/com/hospital/entity/Appointment.java
+++ src/main/java/com/hospital/entity/Appointment.java
@@ -37,7 +37,6 @@ public class Appointment {
         this.status = status;
     }
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
