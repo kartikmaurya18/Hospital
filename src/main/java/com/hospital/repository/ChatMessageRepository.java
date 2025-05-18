01ea6ab5da75b1fdf39acbc0e70f5ebd06a37c1e
navin <kartikmaurya18@gmail.com>
2025-05-18 12:05:34 +0530

    commit

---
+++ src/main/java/com/hospital/repository/ChatMessageRepository.java
@@ -0,0 +1,18 @@
+package com.hospital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hospital.entity.ChatMessage;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    
+    // Fetch all messages between two users (doctor and patient)
+    List<ChatMessage> findBySenderIdAndReceiverIdOrReceiverIdAndSenderId(
+            Long senderId, Long receiverId, Long receiverId2, Long senderId2);
+
+    List<ChatMessage> findBySenderIdAndReceiverIdOrderByTimestampAsc(Long senderId, Long receiverId);
+}
