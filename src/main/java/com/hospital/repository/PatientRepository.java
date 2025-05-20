144b15c4a3d40444de2dbede8713e39949a3d771
navin <kartikmaurya18@gmail.com>
2025-05-20 22:59:35 +0530

    commit

--- src/main/java/com/hospital/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.repository;
-
-import com.hospital.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
